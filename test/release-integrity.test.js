69418ff4446933906d77a58720ff3d2ea5d268e8
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-09-18 22:50:42 +0000

    Avoid ambiguous URL regex in release test

--- test/release-integrity.test.js
+++ test/release-integrity.test.js
@@ -40,5 +40,5 @@ test("README and changelog identify the same current v1.2.5 release", async () =
   assert.match(readme, /Current release:\s*v1\.2\.5/i);
   assert.match(changelog, /\[1\.2\.5\]/);
   assert.match(changelog, /distribution|install|update/i);
-  assert.match(changelog, /voidsmithindustries\.com/i);
+  assert.ok(changelog.includes("voidsmithindustries.com"));
 });
