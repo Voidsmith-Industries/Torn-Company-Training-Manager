05b79e593112ca74f4170de9b487c8b12acb241b
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-09-18 20:51:21 +0100

    Decode text entities in one pass

--- src/core/history.js
+++ src/core/history.js
@@ -14,11 +14,14 @@ function toText(value) {
 }
 
 function decodeTextEntities(text) {
-  return text
-    .replace(/&amp;/gi, "&")
-    .replace(/&quot;/gi, '"')
-    .replace(/&#39;|&#x27;/gi, "'")
-    .replace(/&nbsp;/gi, " ");
+  const entities = Object.freeze({
+    "&amp;": "&",
+    "&quot;": '"',
+    "&#39;": "'",
+    "&#x27;": "'",
+    "&nbsp;": " "
+  });
+  return text.replace(/&(amp|quot|#39|#x27|nbsp);/gi, (entity) => entities[entity.toLowerCase()] ?? entity);
 }
 
 function stripTags(html) {
